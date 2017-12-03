12395265b354b1cc9d9692ce0ac3ff9d4b6c84be
lavelle96 <lavelld@tcd.ie>
2017-12-03 15:48:29 +0000

    db implemented for dir ser

--- src/Client.py
+++ src/Client.py
@@ -2,8 +2,8 @@ import Lib
 import sys
 
 
-Lib.read_file("2.2")
+Lib.read_file("1.1")
 input("Press enter when you want to write this back")
 
 
-Lib.write_file("2.2")
+#Lib.write_file("2.2")
--- src/Directory_Server.py
+++ src/Directory_Server.py
@@ -4,59 +4,114 @@ import requests
 from format import format_file_req
 import sys
 import json
-
+from pymongo import MongoClient
+client = MongoClient()
+db = client.dir_db
+file_map = db.file_map
+dir_map = db.dir_map
+active_nodes = db.active_nodes
+'''
+{
+    'dir':
+    'port':
+}
+'''
 app = Flask(__name__)
 api = Api(app)
 
 #Map of directories to whether they have a file server managing them
 
-DIR_MAP = {
-    'D1':False,
-    'D2':False
+
+FILE_11 = {
+    'name': '1.1',
+    'dir': 'D1'
 }
-FILE_MAP = {
-    '1.1':'D1',
-    '1.2':'D1',
-    '2.1':'D2',
-    '2.2':'D2'
+FILE_12 = {
+    'name': '1.2',
+    'dir': 'D1'
+}
+FILE_21 = {
+    'name': '2.1',
+    'dir': 'D2'
+}
+FILE_22 = {
+    'name': '2.2',
+    'dir': 'D2'
+}
+DIR_1 = {
+    'name': 'D1',
+    'supported': False
+}
+DIR_2 = {
+    'name': 'D2',
+    'supported': False
 }
 #Map of Directory to Node
-ACTIVE_NODES = {}
 
 class Directory_API(Resource):
     def get(self, file_name):
-        if file_name in FILE_MAP.keys() and DIR_MAP[FILE_MAP[file_name]] == True:
-            #url = format_file_req(file_name, ACTIVE_NODES[FILE_MAP[file_name]])
-            #Reformat so that its passed in python map object form and not twice changed into string form
-            node_port = ACTIVE_NODES[FILE_MAP[file_name]]
-            response = {
-                'file_server_port': node_port
-            }
-            return jsonify(response)
-        else:
+        
+        f = file_map.find_one({'name': file_name})
+        if f == None:
             abort(404)
+        file_dir = f['dir']
+        d = dir_map.find_one({'name': file_dir})
+        if d['supported'] == False:
+            abort(404)
+        node = active_nodes.find_one({'dir': file_dir})
+        response = {
+            'file_server_port': node['port']
+        }
+        print('response  :', response)
+        return jsonify(response)
 
 class Node_Init_API(Resource):
     def get(self, port_number):
+        query = dir_map.find()
+        for i in query:
+            print(i)
+
         print("Request received from port: ", port_number)
-        for key in ACTIVE_NODES:
-            if ACTIVE_NODES[key] == port_number:
-                response = {"file_dir": key}
-                return response
-        
-        for dir in DIR_MAP:
-            if DIR_MAP[dir] == False:
-                DIR_MAP[dir] = True
-                ACTIVE_NODES[dir] = port_number
-                print('directory: ' + dir + ' given to port: ' + port_number)
-                response = {"file_dir": dir}
-                return response
-        return None
+        node = active_nodes.find_one({'port': port_number})
+        if node != None:
+            response = {"file_dir": node['dir']}
+            print('response to port ', str(port_number), ' ', response)
+            return jsonify(response)
+
+
+        d = dir_map.find_one({"supported": False})
+        if d==None:
+            return d
+        print('Updating directory: ', d['name'])
+        dir_map.update_one(
+            {"name": d['name']},
+            {
+                '$set':{
+                    "supported": True
+                }
+            }
+        )
+        active_nodes.insert_one({
+            'dir': d['name'],
+            'port': port_number
+        })
+
+        response = {
+            "file_dir": d['name']
+        }
+        print('response to port ', str(port_number), ' ', response)
+        return jsonify(response)
+
 
 
 api.add_resource(Directory_API, '/api/files/<string:file_name>', endpoint = 'file')
 api.add_resource(Node_Init_API, '/api/node/<string:port_number>', endpoint = 'node')
 
 if __name__ == '__main__':
-     server_port = int(sys.argv[1])
-     app.run(host= '0.0.0.0', port = server_port, debug = True)
+    db.drop_collection('file_map')
+    db.drop_collection('dir_map')
+    db.drop_collection('active_nodes')
+    file_map.insert_many([FILE_11, FILE_12, FILE_21, FILE_22])
+    dir_map.insert_many([DIR_1, DIR_2])
+    server_port = int(sys.argv[1])
+    app.run(host= '0.0.0.0', port = server_port, debug = True)
